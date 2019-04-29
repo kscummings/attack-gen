e8e00114e1e188ae0cd66501ba954961f5492e23
KaylaCummings47 <ksc72014@mymail.pomona.edu>
2019-04-29 12:10:12 -0400

    built vae attack generator

--- attack-gen.py
+++ attack-gen.py
@@ -8,18 +8,18 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from sklearn.metrics import confusion_matrix
 from scipy.stats import multivariate_normal
-from sklearn.preprocessing import MinMaxScaler
-from sklearn import svm
+
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.utils import shuffle
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 
-from keras.layers import Dense, BatchNormalization, Activation, Dropout, Conv1D, Flatten, MaxPool1D
+from keras.layers import Input, Lambda, Dense, BatchNormalization, Activation, Dropout, Conv1D, Flatten, MaxPooling1D, UpSampling1D
 from keras.models import Model, Sequential
-from keras.losses import categorical_crossentropy, binary_crossentropy
+from keras.losses import mse, categorical_crossentropy, binary_crossentropy
 from keras.utils import plot_model, to_categorical
 from keras import backend as K
 from keras.regularizers import l2
@@ -37,13 +37,106 @@ import os
 BUILD GENERATOR
 '''
 
+# reparameterization trick
+# instead of sampling from Q(z|X), sample eps = N(0,I)
+# z = z_mean + sqrt(var)*eps
+def sampling(args):
+    """Reparameterization trick by sampling fr an isotropic unit Gaussian.
+    # Arguments:
+        args (tensor): mean and log of variance of Q(z|X)
+    # Returns:
+        z (tensor): sampled latent array of size .... um
+    """
+
+    z_mean, z_log_var = args
+    batch = K.shape(z_mean)[0]
+    dim1 = K.int_shape(z_mean)[1]
+    dim2 = K.int_shape(z_mean)[2]
+    # by default, random_normal has mean=0 and sd=1.0
+    epsilon = K.random_normal(shape=(batch, dim1, dim2))
+    return z_mean + K.exp(0.5 * z_log_var) * epsilon # latent tensor
+
+
+def vae_gen_model(input_shape=(24,36),
+                  k_size=5,
+                  conv=[60,80,36],
+                  resolution=2,
+                  dense=[200,160,480],
+                  latent_dim=100,
+                  pad_type='same',
+                  act_type='relu'):
+    """
+    Build attack generator that takes clean data window as input
+    Architecture based on Chandy's VAE
+    """
+
+    # build encoder
+
+    conv[3] = input_shape[1]
+    inputs = Input(shape=input_shape,name='encoder_input')
+    conv_1 = Conv1D(filters = conv[0], kernel_size = k_size,
+             padding = pad_type, strides = 1)(inputs)
+    batch_1 = BatchNormalization(axis = 2)(conv_1)    # channels along last axis
+    pool_1 = MaxPooling1D(pool_size = resolution,padding = pad_type)(batch_1)
+    conv_2 = Conv1D(filters = conv[1], kernel_size = k_size, padding = pad_type,
+             strides = 1)(pool_1)
+    batch_2 = BatchNormalization(axis = 2)(conv_2)
+    pool_2 = MaxPooling1D(pool_size = resolution,padding = pad_type)(batch_2)
+    dense_1 = Dense(units = dense[0], activation = act_type)(pool_2)
+    batch_3 = BatchNormalization(axis = 2)(dense_1)
+    dense_2 = Dense(units = dense[1], activation = act_type)(batch_3)
+    batch_4 = BatchNormalization(axis = 2)(dense_2)
+    z_mean_un = Dense(units = latent_dim)(batch_4)
+    z_log_var_un = Dense(units = latent_dim, activation = act_type)(batch_4)
+    z_mean = BatchNormalization(axis = 2)(z_mean_un)
+    z_log_var = BatchNormalization(axis = 2)(z_log_var_un)
+
+    # use reparameterization trick to push the sampling out as input
+    # note that "output_shape" isn't necessary with the TensorFlow backend
+    # output shape is (batch_size, 6, latent_dim)
+    z = Lambda(function = sampling, name='z')([z_mean, z_log_var])
+
+    # instantiate encoder
+    encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
+
+    # build decoder
+    latent_inputs = Input(shape=K.int_shape(z)[1:], name='z_sampling')
+    d_dense_1 = Dense(units=dense[1], activation=act_type)(latent_inputs)
+    d_batch_1 = BatchNormalization(axis=2)(d_dense_1)
+    d_dense_2 = Dense(units=dense[0], activation=act_type)(d_batch_1)
+    d_batch_2 = BatchNormalization(axis=2)(d_dense_2)
+    d_dense_3 = Dense(units=dense[2], activation=act_type)(d_batch_2)
+    d_batch_3 = BatchNormalization(axis=2)(d_dense_3)
+    d_up_1 = UpSampling1D(size=resolution)(d_batch_3)
+    d_conv_1 = Conv1D(filters=conv[0], kernel_size = k_size,
+               padding = pad_type, strides = 1)(d_up_1) #data_format = "channels_last"
+    d_batch_4 = BatchNormalization(axis=2)(d_conv_1)
+    d_up_2 = UpSampling1D(size=resolution)(d_batch_4)
+    d_mean_un = Conv1D(filters=conv[2], kernel_size = k_size,
+                padding = pad_type, strides = 1)(d_up_2) # data_format = "channels_last"
+    d_log_var_un = Conv1D(filters=conv[2], kernel_size = k_size,
+                   padding = pad_type, strides = 1)(d_up_2) #data_format = "channels_last"
+    d_mean = BatchNormalization(axis = 2)(d_mean_un)
+    d_log_var = BatchNormalization(axis=2)(d_log_var_un)
+
+    # join the branches by generating random normals. this is our reconstruction
+    outputs = Lambda(function = sampling, name='output')([d_mean, d_log_var])
+
+    # instantiate decoder
+    decoder = Model(latent_inputs, [d_mean,d_log_var,outputs], name='decoder')
+
+    # instantiate vae
+    outputs = decoder(encoder(inputs)[2])[2]
+    vae = Model(inputs, outputs, name='vae')
+
+
 
 def gen_model(num_dense=3,
               input_shape=(24,36),
               reg=.00,
               kernel_initializer='glorot_uniform'):
     """
-    Build an attack generator that takes clean data window as input
+    Build a dumb attack generator that takes clean data window as input
     For now, just some dense layers...
     # Arguments
         num_dense = number of modules in network
