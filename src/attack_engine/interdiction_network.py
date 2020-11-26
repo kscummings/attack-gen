6f6dc07b3e05cb8096c0db5da328d09ea8fbb49f
KaylaCummings47 <ksc72014@mymail.pomona.edu>
2020-11-25 22:06:45 -0500

    built interdiction network

--- src/attack_engine/interdiction_network.py
+++ src/attack_engine/interdiction_network.py
@@ -22,20 +22,45 @@ class InterdictionNetwork:
     Network considered by attacker
     """
     def __init__(self, filepath):
+
+        # water network
         self.original_wn=WaterNetwork(filepath)
+
+        # full week of simulated demand
         self.dem=self.original_wn.sim_demand()
 
+        # just the demands
+        tanks,reservoirs,junctions=self.original_wn.get_nodes()
+        self.dem=self.dem.drop(np.hstack((tanks,reservoirs)),axis=1)
+        totaldem=self.dem.sum(axis=0)
+
+        # build network
         G=self.original_wn.get_network()
 
-        # build the network
-        ### simulate demand to add to each node
-        ### assume supply is equal to total simulated demand (can satisfy all)
-        ### add as network attribute
-        ### add edges:
-        ##### reservoir to all tanks
-        ##### all demand nodes to artificial sink
+        # name source, build reservoir's out-edges to tanks
+        assert ["R1"]==reservoirs
+        assert all([len(G[u][v])==1 for (u,v,_) in G.edges])
+        G.add_node("source")
+        G.add_edge("source","R1","P0") # source to reservoir
+        source_edges=[("R1",tank,"P0") for tank in tanks]
+        G.add_edges_from(source_edges)
 
+        # build sink and its in-edges from junctions with demand
+        G.add_node("sink")
+        sink_edges=[(junction,"sink","P0") for junction in junctions]
+
+        # edge attributes: demand
+        # assumes supply can satisfy all
+        totaldem_dict={(j,s,p):totaldem[j] for (j,s,p) in sink_edges}
+        totaldem_dict.update({("source","R1","P0"):-sum(totaldem_dict.values())})
+        totaldem_dict.update({e : 0 for e in G.edges if (e not in source_edges) & (e not in sink_edges)})
+        nx.set_edge_attributes(G,totaldem_dict,"demand")
+
+        # finalize
         self.topo=G
+        self.source_name="source"
+        self.sink_name="sink"
+
 
     def reduce_demand(self,null_nodes):
         """
@@ -43,7 +68,7 @@ class InterdictionNetwork:
         ### Keyword Arguments
         *`null_nodes` - zero out demand of the nodes in this list
         """
-        dem=nx.get_node_attributes(self.topo,'dem')
+        dem=nx.get_edge_attributes(self.topo,'demand')
 
         # update all of the demands in the network
         nx.set_node_attributes(self.topo,dem)
